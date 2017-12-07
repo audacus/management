8584cc5410c989f0d0f85558f9d06c86f2a6c5a9
audacus <david.burkhart@hotmail.com>
2017-12-07 01:44:50 +0100

    +more example data

--- db/sql/data/data-management.sql
+++ db/sql/data/data-management.sql
@@ -12,9 +12,12 @@ delete from sqlite_sequence where name='thing';
 insert or ignore into thing (name) values ('numberfield')
 	, ('textfield')
 	, ('number')
-	, ('text');
+	, ('text')
+	, ('garage')
+	, ('parking lot');
 -- property
 delete from property;
 delete from sqlite_sequence where name='property';
 insert or ignore into property (name, thing, type, relation) values('value', 1, 3, 1)
-	, ('value', 2, 4, 1);
+	, ('value', 2, 4, 1)
+	, ('parking lots', 5, 6, 2);
